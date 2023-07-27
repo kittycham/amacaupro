54f48e79acd444b376a8db6a28a8e4aae3273fa8
kittycham <71751066+kittycham@users.noreply.github.com>
2023-07-27 16:16:59 +0800

    Update footer.js
    change copyright year

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
             <div className="h-[60px] py-[10px] bg-black">
                 <Container className="flex flex-col lg:flex-row items-center h-full">
                         <div className="text-gray-100 text-sm opacity-40">
-                            Copyright © 2016 曹志強註冊會計師 All rights reserved.&nbsp;|&nbsp;
+                            Copyright © 2023 曹志強註冊會計師 All rights reserved.&nbsp;|&nbsp;
                         </div>
                         <div className="text-gray-100 text-sm opacity-40">Created by{' '} 
                             <a href="https://bottle.mo" title="Powered by WordPress and Gatsby" className="no-underline font-bold text-gray-100 hover:text-white">
